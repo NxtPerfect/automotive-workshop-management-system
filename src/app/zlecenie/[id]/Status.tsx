57897f6137e461e597ad404049c0a18333cb1d7f
NxtPerfect <alakaxan@protonmail.com>
2024-06-02 08:34:12 +0200

    Fixed db error parts null for jobs

--- src/app/zlecenie/[id]/Status.tsx
+++ src/app/zlecenie/[id]/Status.tsx
@@ -6,7 +6,7 @@ export default function Status({status, serviceId, setFinished}: {boolean, numbe
   const router = useRouter();
   return (
     <>
-      <button className="bg-purple-500 hover:bg-purple-600 active:bg-purple-400 transition text-white rounded-md shadow-md mt-2" onChange={() => {
+      <button className="bg-purple-500 hover:bg-purple-600 active:bg-purple-400 transition text-white rounded-md shadow-md mt-2" onClick={() => {
         setFinished(serviceId);
         router.refresh()
       }}>Zakończ</button>
